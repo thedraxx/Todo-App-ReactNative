df1459711c93f9a49257ae853fea5ddb11e48f39
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-15 15:47:01 -0300

    delete completed

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Completed } from './Complete/Completed'
 import { Pending } from './Pending/Pending'
 
 import MyButton from '../../components/buttons/MyButton'
-import { MyTextCompleted, MyTextPending, ViewHome } from './styles'
+import { MyTextPending, ViewHome } from './styles'
 
 const HomeScreen = ({ navigation }: any) => (
     <ViewHome>
-      <MyTextCompleted>Completed Tasks</MyTextCompleted>
-      {/* Component Completed Tasks */}
-      <Completed />
       <MyTextPending>Pending Tasks</MyTextPending>
       {/* Component Pending Tasks */}
       <Pending />
--- src/screens/Home/Pending/Pending.tsx
+++ src/screens/Home/Pending/Pending.tsx
@@ -1,7 +1,6 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import BouncyCheckbox from 'react-native-bouncy-checkbox'
 import React, { useEffect, useState } from 'react'
-import { addTodoCompleted } from '../../../store/slice/TodoSlice'
 import { FlatList } from 'react-native'
 import { MyTextTask, MyTextTaskPending, ViewCompletedTasks, ViewPending } from './styles'
 // import { MyButton } from '../../../components/buttons/MyButton'
@@ -10,12 +9,6 @@ export const Pending = () => {
   const { todos } = useSelector((state: any) => state.todo)
 
   const [Tasks, setTasks] = useState([])
-
-  const dispatch = useDispatch()
-
-  const HandleSelected = (id: number) => {
-    dispatch(addTodoCompleted(id))
-  }
 
   useEffect(() => {
     setTasks(todos)
@@ -37,9 +30,6 @@ export const Pending = () => {
               <BouncyCheckbox
                 isChecked={false}
                 style={{ padding: 10 }}
-                onPress={() => {
-                  HandleSelected(item._id)
-                }}
               />
               <MyTextTask>{item.title}</MyTextTask>
 
